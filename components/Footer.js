8290fdfce2e3f250f13089da4d9e0ed12255c784
Muthumeena Renganathan <muthumeena.R@roboticodigital.com>
2026-05-25 19:06:24 +0530

    added

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="bg-black text-white text-center py-10">
       <p>
-        © 2026 ChillWall. All rights reserved.
+        © 2026 Chill Wall refrigeration India LLP. All rights reserved.
       </p>
     </footer>
   )
